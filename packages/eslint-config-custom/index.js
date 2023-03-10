16d30dc8ba52874a6d7704f561cad1082ee6b0df
Ion Cretu <cretuion94@gmail.com>
2023-03-10 20:54:31 +0200

    clean gitignore; add user-list proj;

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,11 +4,11 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   ignorePatterns: ["node_modules", "dist"],
-  // parserOptions: {
-  //   babelOptions: {
-  //     presets: [require.resolve("next/babel")],
-  //   },
-  // },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
